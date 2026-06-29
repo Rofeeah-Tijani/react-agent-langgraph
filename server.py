1a8f766aab49d53975e2979199c8b515ba448c9b
Rofeeah-Tijani <rofiattijani.iotbtech@hotmail.com>
2026-06-29 21:42:04 +0100

    Fix server app definition

--- server.py
+++ server.py
@@ -1,3 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from agent import agent
+from langchain_core.messages import HumanMessage
+
+
+app = FastAPI(
+    title="AI Agent API"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "AI Agent API is running",
+        "docs": "/docs"
+    }
+
+
 @app.post("/chat")
 def chat(message: str):
 
@@ -19,11 +48,9 @@ def chat(message: str):
 
     messages = result["messages"]
 
-
     answer = ""
 
     for msg in reversed(messages):
-
         if msg.content:
             answer = msg.content
             break
@@ -32,3 +59,14 @@ def chat(message: str):
     return {
         "response": answer
     }
+
+
+if __name__ == "__main__":
+
+    import uvicorn
+
+    uvicorn.run(
+        "server:app",
+        host="0.0.0.0",
+        port=8000
+    )
